37e8bcb8008fbd01025cfd55537ec94a7af6c552
Shadi <ra.rahdari@gmail.com>
2024-09-29 12:08:02 +0300

    fix: enabled cors

--- app.js
+++ app.js
@@ -5,11 +5,17 @@ const { getProductList } = require("./routers/products");
 const db = require("./models/database");
 const logger = require("./logger/logger");
 const helmet = require("helmet");
-const cors = require("cors");
+
+const corsOptions = {
+  origin: ["http://localhost:3000", "http://127.0.0.1:3000"], // Add other ports as needed
+  methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+};
+
+app.use(cors(corsOptions));
 //const cors = require("cors");
 
 //const cors = require("cors");
-//app.use(cors());
 // import { productModel } from /... <-- best
 // expot const productModel = () => {...}
 // import productModeeeel from ...
@@ -19,12 +25,12 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(
-    helmet.contentSecurityPolicy({
-        directives: {
-            defaultSrc: ["'self'"],
-            scriptSrc: ["'self'", "'unsafe-inline'"],
-        },
-    })
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+    },
+  })
 );
 app.use(cors());
 
@@ -36,258 +42,243 @@ productModel.createOrderDetailsTable();
 
 //get all products
 app.get("/api/products", async (req, res) => {
-    logger.info(
-        `Received GET request to /api/products with query parameters: ${JSON.stringify(
-            req.query
-        )}`
-    );
+  logger.info(
+    `Received GET request to /api/products with query parameters: ${JSON.stringify(
+      req.query
+    )}`
+  );
 
-    const filter = {
-        product_type: req.query.product_type,
-        color: req.query.color,
-        brand: req.query.brand,
-        priceRange: {
-            min: req.query.min_price ? parseInt(req.query.min_price) : 0,
-            max: req.query.max_price ? parseInt(req.query.max_price) : Infinity,
-        },
-        sort_by: req.query.sort_by,
-        order: req.query.order,
-    };
-    try {
-        const products = await new Promise((resolve, reject) => {
-            getProductList(filter, (err, result) =>
-                err ? reject(err) : resolve(result)
-            );
-        });
-        logger.info(`Sent ${products.length} products in response`);
-        res.send(products);
-    } catch (err) {
-        logger.error(`Error retrieving products: ${err.message}`);
-        res.status(500).send("Error retrieving products.");
-    }
+  const filter = {
+    product_type: req.query.product_type,
+    color: req.query.color,
+    brand: req.query.brand,
+    priceRange: {
+      min: req.query.min_price ? parseInt(req.query.min_price) : 0,
+      max: req.query.max_price ? parseInt(req.query.max_price) : Infinity,
+    },
+    sort_by: req.query.sort_by,
+    order: req.query.order,
+  };
+  try {
+    const products = await new Promise((resolve, reject) => {
+      getProductList(filter, (err, result) =>
+        err ? reject(err) : resolve(result)
+      );
+    });
+    logger.info(`Sent ${products.length} products in response`);
+    res.send(products);
+  } catch (err) {
+    logger.error(`Error retrieving products: ${err.message}`);
+    res.status(500).send("Error retrieving products.");
+  }
 });
 
 // API endpoint to gett order with order_id
 app.get("/", (req, res) => {
-    res.send("E-shop backend");
+  res.send("E-shop backend");
 });
 app.get("/api/products/:id", (req, res) => {
-    const productId = req.params.id;
-    //Call function to get info about the product
-    productModel.getProductById(productId, (err, product) => {
-        if (err) {
-            return res
-                .status(500)
-                .send("Error retrieving product from the database.");
-        }
+  const productId = req.params.id;
+  //Call function to get info about the product
+  productModel.getProductById(productId, (err, product) => {
+    if (err) {
+      return res
+        .status(500)
+        .send("Error retrieving product from the database.");
+    }
 
-        if (!product) {
-            return res.status(404).send("Product not found.");
-        }
-        // Send the product details as a response
-        res.send({
-            success: true,
-            message: "Product details:",
-            product: {
-                id: product.product_id,
-                product: product.product_type,
-                brand: product.brand,
-                title: product.title,
-                picture: product.picture,
-                color: product.color,
-                price: product.price,
-                shortDescription: product.shortDescription,
-                stockAmount: product.stockAmount,
-                popularity: product.popularity_score,
-            },
-        });
-        productModel.increasePopularityScore(productId, (err) => {
-            if (err) {
-                return res
-                    .status(500)
-                    .send("Error updating product popularity.");
-            }
-        });
+    if (!product) {
+      return res.status(404).send("Product not found.");
+    }
+    // Send the product details as a response
+    res.send({
+      success: true,
+      message: "Product details:",
+      product: {
+        id: product.product_id,
+        product: product.product_type,
+        brand: product.brand,
+        title: product.title,
+        picture: product.picture,
+        color: product.color,
+        price: product.price,
+        shortDescription: product.shortDescription,
+        stockAmount: product.stockAmount,
+        popularity: product.popularity_score,
+      },
     });
+    productModel.increasePopularityScore(productId, (err) => {
+      if (err) {
+        return res.status(500).send("Error updating product popularity.");
+      }
+    });
+  });
 });
 
 app.get("/api/stock/:id", (req, res) => {
-    const productId = req.params.id;
-    productModel.getProductById(productId, (err, product) => {
-        if (err) {
-            return res.status(500).send("Error retrieving product.");
-        }
-        if (!product) {
-            return res.status(404).send("Product not found.");
-        }
-        res.send({ stockAmount: product.stockAmount });
-    });
+  const productId = req.params.id;
+  productModel.getProductById(productId, (err, product) => {
+    if (err) {
+      return res.status(500).send("Error retrieving product.");
+    }
+    if (!product) {
+      return res.status(404).send("Product not found.");
+    }
+    res.send({ stockAmount: product.stockAmount });
+  });
 });
 
 app.get("/api/orders/:id", (req, res) => {
-    const orderId = req.params.id;
+  const orderId = req.params.id;
 
-    // Call the getOrderById function
-    productModel.getOrderById(orderId, (err, order) => {
-        if (err) {
-            return res
-                .status(500)
-                .send("Error retrieving order from the database.");
-        }
+  // Call the getOrderById function
+  productModel.getOrderById(orderId, (err, order) => {
+    if (err) {
+      return res.status(500).send("Error retrieving order from the database.");
+    }
 
-        if (!order) {
-            return res.status(404).send("Order not found.");
-        }
+    if (!order) {
+      return res.status(404).send("Order not found.");
+    }
 
-        // Send the order details as a response
-        res.send({
-            success: true,
-            order: {
-                orderId: order.order_id,
-                user: {
-                    id: order.user_id,
-                    name: order.user_name,
-                    surname: order.user_surname,
-                    email: order.user_email,
-                    phone: order.user_phone,
-                },
-                product: {
-                    id: order.product_id,
-                    title: order.title,
-                    brand: order.brand,
-                    color: order.color,
-                    price: order.price,
-                },
+    // Send the order details as a response
+    res.send({
+      success: true,
+      order: {
+        orderId: order.order_id,
+        user: {
+          id: order.user_id,
+          name: order.user_name,
+          surname: order.user_surname,
+          email: order.user_email,
+          phone: order.user_phone,
+        },
+        product: {
+          id: order.product_id,
+          title: order.title,
+          brand: order.brand,
+          color: order.color,
+          price: order.price,
+        },
 
-                order_created_date: order.order_created_date,
-                order_modify_date: order.order_modify_date,
-                order_status: order.order_status,
-            },
-        });
+        order_created_date: order.order_created_date,
+        order_modify_date: order.order_modify_date,
+        order_status: order.order_status,
+      },
     });
+  });
 });
 
 app.post("/api/orders", (req, res) => {
-    const { productId, userName, userSurname, userEmail, userPhone } = req.body;
+  const { productId, userName, userSurname, userEmail, userPhone } = req.body;
 
-    // Validation of entered data
+  // Validation of entered data
 
-    if (!productId) {
-        return res.status(400).send("Product ID and valid color are required");
+  if (!productId) {
+    return res.status(400).send("Product ID and valid color are required");
+  }
+  if (!userName || !userSurname || !userEmail || !userPhone) {
+    return res.status(400).send("All user fields are required");
+  }
+
+  // Getting product data from the database
+
+  const query = "SELECT * FROM products WHERE product_id = ?";
+
+  db.get(query, [productId], (err, product) => {
+    if (err) {
+      return res
+        .status(500)
+        .send("Error retrieving product from the database.");
     }
-    if (!userName || !userSurname || !userEmail || !userPhone) {
-        return res.status(400).send("All user fields are required");
+
+    if (!product) {
+      return res.status(404).send("Product not found.");
     }
 
-    // Getting product data from the database
+    // Checking whether there is a sufficient quantity of goods in stock
 
-    const query = "SELECT * FROM products WHERE product_id = ?";
+    if (product.stockAmount < 1) {
+      return res.status(400).send("Not enough products in stock.");
+    }
 
-    db.get(query, [productId], (err, product) => {
+    // If the product is in stock, create a user
+
+    const userQuery =
+      "INSERT INTO users (user_name, user_surname, user_email, user_phone, user_role) VALUES (?, ?, ?, ?, ?)";
+    db.run(
+      userQuery,
+      [userName, userSurname, userEmail, userPhone, "customer"],
+      function (err) {
         if (err) {
-            return res
-                .status(500)
-                .send("Error retrieving product from the database.");
+          return res.status(500).send("Error saving user to the database.");
         }
 
-        if (!product) {
-            return res.status(404).send("Product not found.");
-        }
+        const userId = this.lastID;
 
-        // Checking whether there is a sufficient quantity of goods in stock
+        // userId
+        console.log("New User ID:", userId);
 
-        if (product.stockAmount < 1) {
-            return res.status(400).send("Not enough products in stock.");
-        }
+        // Creating an order in the database
 
-        // If the product is in stock, create a user
+        const orderQuery = "INSERT INTO orders (user_id) VALUES (?)";
+        db.run(orderQuery, [userId], function (err) {
+          if (err) {
+            return res.status(500).send("Error creating order.");
+          }
 
-        const userQuery =
-            "INSERT INTO users (user_name, user_surname, user_email, user_phone, user_role) VALUES (?, ?, ?, ?, ?)";
-        db.run(
-            userQuery,
-            [userName, userSurname, userEmail, userPhone, "customer"],
-            function (err) {
-                if (err) {
-                    return res
-                        .status(500)
-                        .send("Error saving user to the database.");
-                }
+          const orderId = this.lastID;
 
-                const userId = this.lastID;
+          // Adding order details
 
-                // userId
-                console.log("New User ID:", userId);
-
-                // Creating an order in the database
-
-                const orderQuery = "INSERT INTO orders (user_id) VALUES (?)";
-                db.run(orderQuery, [userId], function (err) {
-                    if (err) {
-                        return res.status(500).send("Error creating order.");
-                    }
-
-                    const orderId = this.lastID;
-
-                    // Adding order details
-
-                    const orderDetailsQuery = `
+          const orderDetailsQuery = `
                     INSERT INTO orderDetails (order_id, product_id, total_price, order_created_date, order_modify_date, order_status)
                     VALUES (?, ?, ?, datetime('now'), datetime('now'), ?)
                 `;
-                    const totalPrice = product.price * 1; // Assuming a default quantity of 1
-                    db.run(
-                        orderDetailsQuery,
-                        [orderId, productId, totalPrice, "confirmed"],
-                        (err) => {
-                            if (err) {
-                                return res
-                                    .status(500)
-                                    .send("Error saving order details.");
-                            }
-                            console.log("New order: userId:", userId);
+          const totalPrice = product.price * 1; // Assuming a default quantity of 1
+          db.run(
+            orderDetailsQuery,
+            [orderId, productId, totalPrice, "confirmed"],
+            (err) => {
+              if (err) {
+                return res.status(500).send("Error saving order details.");
+              }
+              console.log("New order: userId:", userId);
 
-                            // Updating the quantity of goods in the database
+              // Updating the quantity of goods in the database
 
-                            const updateStockQuery =
-                                "UPDATE products SET stockAmount = stockAmount - 1 WHERE product_id = ?";
-                            db.run(updateStockQuery, [productId], (err) => {
-                                if (err) {
-                                    return res
-                                        .status(500)
-                                        .send("Error updating product stock.");
-                                }
-                                console.log(productId, product.stockAmount);
+              const updateStockQuery =
+                "UPDATE products SET stockAmount = stockAmount - 1 WHERE product_id = ?";
+              db.run(updateStockQuery, [productId], (err) => {
+                if (err) {
+                  return res.status(500).send("Error updating product stock.");
+                }
+                console.log(productId, product.stockAmount);
 
-                                productModel.increasePopularityScore(
-                                    productId,
-                                    (err) => {
-                                        if (err) {
-                                            return res
-                                                .status(500)
-                                                .send(
-                                                    "Error updating product popularity."
-                                                );
-                                        }
-                                    }
-                                );
-
-                                // Sending a response to the client
-
-                                res.send({
-                                    success: true,
-                                    message:
-                                        "Order successfully created, stock updated, popularity updated",
-                                    orderId: orderId,
-                                    userId: userId,
-                                });
-                            });
-                        }
-                    );
+                productModel.increasePopularityScore(productId, (err) => {
+                  if (err) {
+                    return res
+                      .status(500)
+                      .send("Error updating product popularity.");
+                  }
                 });
+
+                // Sending a response to the client
+
+                res.send({
+                  success: true,
+                  message:
+                    "Order successfully created, stock updated, popularity updated",
+                  orderId: orderId,
+                  userId: userId,
+                });
+              });
             }
-        );
-    });
+          );
+        });
+      }
+    );
+  });
 });
 
 //port env
